c19b2ac0c614bf6bcb59be1ba716be0ff8fd938f
ddas-04 <59819093+ddas-04@users.noreply.github.com>
2020-04-27 20:58:11 +0800

    Triangular wave generation python code

--- Triangular_wave.py
+++ Triangular_wave.py
@@ -1,15 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams.update({'font.size':15})
+plt.rcParams.update({'font.size':25})
 
 A=input('Enter amplitude : ')
-T=input('Enter Time period : ')
+T=input('Enter Time period (ms) : ')
 T=float(T)
+f=10**3/T 
+print('Frequency = ' + str(f) + ' kHz')
 n=2001
 t=np.linspace(0,T,n)
 signal=np.zeros(n)
 Total_signal=[]
 Total_time=[]
+########### Creating Triangular wave for a single time period ################
 for i in range(len(t)):
 	if t[i]<T/4.0:
 		signal[i]=(4*A/T)*t[i]
@@ -17,22 +20,23 @@ for i in range(len(t)):
 		signal[i]=(-4*A/T)*t[i]+2*A
 	if t[i]>=(3*T/4.0) and t[i]<=T:
 		signal[i]=(4*A/T)*t[i]-4*A
-cycle=input('Enter number of cycle of the input signal : ')
+####################################################################
+cycle=input('Enter number of cycle of the input signal : ') 
+############ Repeating the wave for the given number of cycles ##############
 for rep in range(cycle):
 	Total_signal=np.append(Total_signal, signal)
 	Total_time=np.append(Total_time,t+rep*T)
-
+####################################################################
 Total_length=len(Total_time)
 Crit_value=input('Enter critical value : ')
-Crit_value=Crit_value*np.ones(Total_length)
-'''
-plt.subplot(2,1,1)
-plt.plot(t,signal)
-plt.grid()
-'''
-#plt.subplot(2,1,2)
-plt.plot(Total_time,Total_signal)
-plt.plot(Total_time,Crit_value)
+Crit_value=Crit_value*np.ones(Total_length) # creating critical value array
+###################### Plot the signal #################################
+fig = plt.figure(figsize=(14,9))
+plt.plot(Total_time,Total_signal,linewidth = 3.0, label='Signal')
+plt.plot(Total_time,Crit_value, linewidth = 3.0, label='Crit Value')
+plt.xlabel('Time(ms)')
+plt.ylabel('Signal ')
+plt.legend(loc='upper right')
 plt.grid()
 plt.show()
 		
